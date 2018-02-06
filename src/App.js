8ac4d7925a24d26cf7814b580fcaa13857e4fb9b
balg <balazs.jilling@gmail.com>
2018-02-06 18:22:21 +0100

    UserOutput prop handling

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <UserOutput />
-        <UserOutput />
-        <UserOutput />
+        <UserOutput username="balg" />
+        <UserOutput username="jozsika" />
+        <UserOutput username="marika" />
       </div>
     );
   }
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const userOutput = () => {
+const userOutput = ( props ) => {
     return (
         <div className="userOutput">
-            <p>This is the first paragraph.</p>
+            <p>Hi there, {props.username}!</p>
             <p>This is the second paragraph.</p>
         </div>
     );
