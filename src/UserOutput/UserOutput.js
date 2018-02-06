7801d004f2f2974829d5ac130dc8a201268b0db8
balg <balazs.jilling@gmail.com>
2018-02-06 18:17:21 +0100

    UserOutput created and added to App

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div className="userOutput">
+            <p>This is the first paragraph.</p>
+            <p>This is the second paragraph.</p>
+        </div>
+    );
+}
+
+export default userOutput;
