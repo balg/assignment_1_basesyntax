6bef9834823abfbd330f7cb0c105c0a750c92d72
balg <balazs.jilling@gmail.com>
2018-02-07 16:07:49 +0100

    Some styling added

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const userInput = ( props ) => {
+
+    const style = {
+        margin: "20px auto",
+        width: "50%",
+        padding: "5px 10px",
+        border: "solid 1px blue",
+    };
+
     return(
         <div className="userInput">
-            <input type="text" onChange={ props.onChange } value={ props.userName } />
+            <input 
+                type="text" 
+                style={ style }
+                onChange={ props.onChange } 
+                value={ props.userName } />
         </div>
     );
 };
